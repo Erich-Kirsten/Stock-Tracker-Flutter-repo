b8c019774542b7eae817f322e5bf9e61f33aeff0
Erich Kirsten <erikir2004@hotmail.com>
2025-07-01 14:11:39 +0200

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'Screens/auth_page.dart';
+import 'Screens/navigation_controller.dart';
+import 'Screens/news_page.dart';
+import 'models/user_details.dart';
+import 'Screens/splash_page.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(const MyApp());
+}
+
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'StockTrack App',
+      debugShowCheckedModeBanner: false,
+      initialRoute: '/',
+      routes: {
+        '/': (context) => const SplashPage(),         // 🔁 NEW ENTRY POINT
+        '/login': (context) => const AuthPage(),
+        '/news': (context) {
+          // final args = ModalRoute.of(context)!.settings.arguments as UserDetails;
+          return NavigationController(); // ✅ Still used
+        },
+      },
+    );
+  }
+}
